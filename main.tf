31df33ed4c55bcf4cbc3c9b2fe0de188ff23b322
Talita Oliveira <litaa.oliveira@gmail.com>
2025-05-24 18:16:25 +0200

    Subnet definition - public and private

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 locals {
     vpc_name = "${var.env_name} ${var.vpc_name}"
-    cluster_name = "${var.cluster_name} ${var.env_name}"
+    cluster_name = "${var.cluster_name}-${var.env_name}"
 }
 
 ## AWS VPC definition
@@ -13,5 +13,71 @@ resource "aws_vpc" "main" {
     tags = {
         "Name"                                          = local.vpc_name,
         "kubernetes.io/cluster/${local.cluster_name}"   = "shared"
+    }
+}
+
+# subnet definition
+
+data "aws_availability_zones" "available" { # "Asking" AWS for AZ IDs in the region we specified
+    state = "available"
+}
+
+resource "aws_subnet" "public-subnet-a" {
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = var.public_subnet_a_cidr
+    availability_zone   = data.aws_availability_zones.available.names[0]
+
+    tags = {
+        "Name"                                          = (
+            "${local.vpc_name}-public-subnet-a"
+        )
+        "kubernetes.io/cluster/${local.cluster_name}"   = "shared"
+        "kubernetes.io/role/elb"                        = 1
+            
+    }
+}
+
+resource "aws_subnet" "public-subnet-b" {
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = var.public_subnet_b_cidr
+    availability_zone   = data.aws_availability_zones.available.names[1]
+
+    tags = {
+        "Name"                                          = (
+            "${local.vpc_name}-public-subnet-b"
+        )
+        "kubernetes.io/cluster/${local.cluster_name}"   = "shared"
+        "kubernetes.io/role/elb"                        = 1
+            
+    }
+}
+
+resource "aws_subnet" "private-subnet-a" {
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = var.private_subnet_a_cidr
+    availability_zone   = data.aws_availability_zones.available.names[0]
+
+    tags = {
+        "Name"                                          = (
+            "${local.vpc_name}-private-subnet-a"
+        )
+        "kubernetes.io/cluster/${local.cluster_name}"   = "shared"
+        "kubernetes.io/role/internal-elb"               = 1
+            
+    }
+}
+
+resource "aws_subnet" "private-subnet-b" {
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = var.private_subnet_b_cidr
+    availability_zone   = data.aws_availability_zones.available.names[1]
+
+    tags = {
+        "Name"                                          = (
+            "${local.vpc_name}-private-subnet-b"
+        )
+        "kubernetes.io/cluster/${local.cluster_name}"   = "shared"
+        "kubernetes.io/role/internal-elb"               = 1
+            
     }
 }
